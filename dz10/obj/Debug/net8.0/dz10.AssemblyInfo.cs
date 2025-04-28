21883a8b4bd9fc01a79269a1d4e0e411acdcb6f8
Шпаков Данил <shpakovvdanill@mail.ru>
2025-04-28 19:48:54 +0700

    End v2

--- dz10/obj/Debug/net8.0/dz10.AssemblyInfo.cs
+++ dz10/obj/Debug/net8.0/dz10.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dz10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c49e547ebfa2d00d0b19783003a9f9c4cf0833a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+413f32e3992bbe0389b8db4cde361bf240ae10e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("dz10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dz10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
